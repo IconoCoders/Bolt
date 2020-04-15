764be02ca6de31affbf6dd4f8e0684c9d181633e
kovacsdanielakos <git@progweb.hu>
2020-04-15 14:00:26 +0200

    Simple 404

---
+++ web/app/themes/bolt/404.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+
+		<main id="main" class="site-main" role="main">
+
+			<div class="error-404 not-found">
+
+				<div class="page-content">
+
+					<header class="page-header">
+						<h1 class="page-title"><?php esc_html_e( '404', 'storefront' ); ?></h1>
+					</header><!-- .page-header -->
+
+					<?php
+					echo '<section aria-label="' . esc_html__( 'Search', 'storefront' ) . '">';
+
+					if ( storefront_is_woocommerce_activated() ) {
+						the_widget( 'WC_Widget_Product_Search' );
+					} else {
+						get_search_form();
+					}
+
+					echo '</section>';
+					
+					?>
+
+				</div><!-- .page-content -->
+			</div><!-- .error-404 -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
